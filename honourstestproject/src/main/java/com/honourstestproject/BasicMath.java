8827342ec417a66981924ab136eda5c2472ac6db
danratinac <danratinac@gmail.com>
2023-07-24 22:20:30 +1000

    initialised java project with maven

---
+++ honourstestproject/src/main/java/com/honourstestproject/BasicMath.java
@@ -0,0 +1,19 @@
+package com.honourstestproject;
+
+public class BasicMath {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int b, int a) {
+        return b - a;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ honourstestproject/src/test/java/com/honourstestproject/AppTest.java
@@ -0,0 +1,38 @@
+package com.honourstestproject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+
+    BasicMath math = new BasicMath();
+
+    @Test
+    void testAdd() {
+        assertEquals(2, math.add(1, 1));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(1, math.subtract(2, 1));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(4, math.add(2, 2));
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(2, math.add(4, 2));
+    }
+
+    @Test
+    void failTest() {
+        assertEquals(1, 2);
+    }
+}
